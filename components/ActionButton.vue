213e6e483e40d47319040426e16ad75c1c9c3f02
Robin Hellemans <robin.hellemans@partners.basf.com>
2024-01-19 15:31:20 +0100

    add animation to dialog

--- components/ActionButton.vue
+++ components/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :type="type"
-    class="relative border-4 border-yellow-300 px-4 py-2 before:absolute before:inset-0 before:z-[-1] before:origin-left before:scale-x-0 before:bg-yellow-300 before:transition-transform hover:before:scale-x-100 focus-visible:before:scale-x-100"
+    class="relative rounded border-4 border-yellow-300 px-4 py-2 before:absolute before:inset-0 before:z-[-1] before:origin-left before:scale-x-0 before:bg-yellow-300 before:transition-transform hover:before:scale-x-100 focus-visible:before:scale-x-100"
   >
     <slot />
   </button>
--- components/ContactDialog.vue
+++ components/ContactDialog.vue
@@ -1,10 +1,10 @@
 <template>
   <dialog
-    class="w-11/12 flex-col gap-2 border-2 border-black p-4 sm:w-[500px]"
+    class="w-11/12 flex-col gap-2 border-2 border-black p-4 backdrop-opacity-5 sm:w-[500px]"
     ref="dialogEl"
     :class="classes"
   >
-    <ContactForm v-if="open" />
+    <ContactForm />
 
     <button type="button" @click="emit('onClose')">Close</button>
   </dialog>
@@ -29,3 +29,54 @@ watch(
   },
 );
 </script>
+
+// Animating backdrop. This seems impossible with Tailwind // Inspired by
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
+<style lang="scss" scoped>
+$transition-duration: 500ms;
+
+dialog[open] {
+  opacity: 1;
+  transform: translateY(0%);
+}
+
+dialog {
+  opacity: 0;
+  transform: translateY(-50%);
+  transition:
+    opacity $transition-duration ease-out,
+    transform $transition-duration ease-out,
+    overlay $transition-duration ease-out allow-discrete,
+    display $transition-duration ease-out allow-discrete;
+}
+
+/*   Before-open state  */
+/* Needs to be after the previous dialog[open] rule to take effect,
+    as the specificity is the same */
+@starting-style {
+  dialog[open] {
+    opacity: 0;
+    transform: translateY(-50%);
+  }
+}
+
+/* Transition the :backdrop when the dialog modal is promoted to the top layer */
+dialog::backdrop {
+  --transition-duration: 300ms;
+  background-color: rgb(0 0 0 / 0%);
+  transition:
+    display $transition-duration allow-discrete,
+    overlay $transition-duration allow-discrete,
+    background-color $transition-duration;
+}
+
+dialog[open]::backdrop {
+  background-color: rgb(0 0 0 / 25%);
+}
+
+@starting-style {
+  dialog[open]::backdrop {
+    background-color: rgb(0 0 0 / 0%);
+  }
+}
+</style>
