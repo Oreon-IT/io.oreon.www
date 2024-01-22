2c08120c1dc6e9ea8f968ca3f251d06bb975c825
Robin Hellemans <robin.hellemans@partners.basf.com>
2024-01-22 09:06:09 +0100

    add favicon & company logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="flex h-full flex-col">
-    <a
-      href="https://github.com/Oreon-IT/io.oreon.www"
-      target="_blank"
-      class="self-end"
-    >
-      <GitHubIcon width="32" />
-    </a>
+    <header class="flex items-start justify-between">
+      <img
+        src="@/assets/logo.png"
+        alt="Logo for Oreon"
+        width="76"
+        height="55"
+      />
+      <a href="https://github.com/Oreon-IT/io.oreon.www" target="_blank">
+        <GitHubIcon width="32" />
+      </a>
+    </header>
     <div class="grid grow place-content-center">
       <h1 class="mb-8 text-center text-6xl">
         Hello there,
